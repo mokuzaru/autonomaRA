3bc233e6df6aa2bd2efa2cead1e820ca933699db
mokuzaru <kenny.otoya@pucp.pe>
2019-07-03 15:36:18 -0500

    upload

---
+++ Assets/cambio.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cambio : MonoBehaviour
+{
+    public GameObject imagen;
+    public GameObject prefab;
+      public GameObject imagen2;
+
+    public void Imagen(){
+      //Instantiate(prefab, prefab.transform.SetParent(imagen.transform),Quaternion.identity);
+      prefab.SetActive(true);
+      //Destroy(imagen);
+      imagen.SetActive(true);
+      imagen2.SetActive(false);
+    }
+}
---
+++ Assets/ray.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.SceneManagement;
+
+public class ray : MonoBehaviour{
+
+	//public GameObject player;
+	//public GameObject prefab;
+	//Color red = Color.red;
+	void Start(){
+
+  }
+
+
+	void Update(){
+		if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began){
+			Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+			RaycastHit Hit;
+
+			if(Physics.Raycast(ray, out Hit)){
+					string Nombre = Hit.transform.name;
+					switch(Nombre){
+						case "cap1":
+							Debug.Log(Nombre);
+							//SceneManager.LoadScene("end");
+							//Destroy(player,7);
+							//Instantiate(prefab, new Vector3(0,5,0),Quaternion.identity);
+						break;
+					}
+			}
+		}
+  }
+}
